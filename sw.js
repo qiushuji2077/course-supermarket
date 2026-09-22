df7a5058df4cd89a16829aaf62d9d04afd62646d
小瑞瑞 <xiaoruirui@xiaoruiruideMacBook-Air.local>
2026-09-22 15:16:00 +0800

    Review existing course quality and add 44 screened courses

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 课程超市 PWA：每次打开网络优先拉货架。 */
-const SHELF_VERSION = '20260922-quality2';
+const SHELF_VERSION = '20260922-quality3';
 const PRECACHE = 'cs-' + SHELF_VERSION;
 
 self.addEventListener('install', () => {
